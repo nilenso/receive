60208d7e56c7e74624870e22ae719b6699361b30
danySam <danisam17@gmail.com>
2020-03-18 03:29:38 +0530

    add ping route to the http server

--- src/receive/core.clj
+++ src/receive/core.clj
@@ -1,6 +1,11 @@
-(ns receive.core)
+(ns receive.core
+  (:require [ring.adapter.jetty :as jetty]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn handler [request]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "Receive - Work in Progress"})
+
+(defn -main [& args]
+  (jetty/run-jetty handler {:port  3000
+                      :join? false}))
