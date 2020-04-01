6dffe43eb2af2b554371db81adb9244c28387755
danySam <danisam17@gmail.com>
2020-04-02 01:31:43 +0530

    add test for upload handler

--- test/receive/core_test.clj
+++ test/receive/core_test.clj
@@ -1,7 +1,8 @@
 (ns receive.core-test
   (:require [clojure.test :refer :all]
             [receive.core :as core]
-            [ring.mock.request :as mock]))
+            [ring.mock.request :as mock]
+            [clojure.java.io :as io]))
 
 (deftest ping-handler
   (is (= (core/ping (mock/request :get "/ping"))
@@ -14,3 +15,22 @@
          {:status 404
           :body {:success false
                  :message "Not found"}})))
+
+(defn create-temp-file [file]
+  (with-open [file (io/writer file)]
+    (.write file "Some data"))
+  (io/file file))
+
+(deftest upload-handler
+  (let [tempfile (create-temp-file "/tmp/tempfile.dat")
+        file {:tempfile tempfile
+              :content-type "text/plain"
+              :filename "tempfile.dat"}]
+    (is (= (core/upload
+            (-> (mock/request :post "/upload/")
+                (assoc :content-type "multipart/form-data"
+                       :params {"file" file}
+                       :multipart-params {"file" file}
+                       :body (io/input-stream (:tempfile file)))))
+           {:status 200
+            :body {:name "tempfile.dat"}}))))
