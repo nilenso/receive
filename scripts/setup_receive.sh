360c0c494f33425cd22f693b8668eb5de369cd65
danySam <danisam17@gmail.com>
2020-04-23 00:39:25 +0530

    pipeline: add staging configuration for deployment

---
+++ scripts/setup_receive.sh
@@ -0,0 +1,27 @@
+env="staging"
+if [ $1 = "production" ];
+    then
+        env="production"
+fi
+
+echo "\nSetting up [$env]"
+
+echo "Cloning Repository"
+git clone https://gitlab.com/nilenso/receive.git --quiet
+
+echo "Copying files"
+rm -rf /opt/$env/
+mkdir /opt/$env/
+mv receive /opt/$env/
+cd /opt/$env/receive
+cp /opt/database.edn /opt/$env/receive/resources/
+
+echo "Running lein deps"
+lein deps > /dev/null
+
+echo "Running database migration"
+lein migrate
+
+echo "Restarting service"
+systemctl restart receive.$env.service
+echo "Setup Completed"
