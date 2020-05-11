b8b7c03ba617070bacdc0be7bb1c7a04553d87ce
danySam <danisam17@gmail.com>
2020-05-11 13:42:42 +0530

    add upload validations to ui

--- resources/public/js/main.js
+++ resources/public/js/main.js
@@ -3,10 +3,30 @@ function getFile() {
 }
 
 function uploadFile(obj) {
-    const file = obj.value
-    const [fileName] = file.split("\\").slice(-1)
+    const MAX_FILE_SIZE = window.config['max-file-size']
+    const MAX_FILENAME_LENGTH = window.config['max-filename-length']
+
+    const [file] = obj.files
+    if (!file)
+        return showUploadError("No file selected!")
+
+    const { name, size } = file
+    if (size == 0)
+        return showUploadError("File size is too small!")
+    if (size > MAX_FILE_SIZE)
+        return showUploadError("File size too big!")
+    if (!name || name.length >= MAX_FILENAME_LENGTH)
+        return showUploadError("File name too long!")
+
     const uploadInput = document.getElementsByClassName("upload-input")
-    uploadInput.innerHTML = fileName
+    uploadInput.innerHTML = name
     document.myForm.submit()
     event.preventDefault()
+}
+
+function showUploadError(message) {
+    const [uploadError] = document.getElementsByClassName("upload-error")
+    uploadError.innerHTML = message
+    uploadError.classList.add('show')
+    setTimeout(() => uploadError.classList.remove('show'), 3e3)
 }
--- src/receive/view/base.clj
+++ src/receive/view/base.clj
@@ -1,6 +1,15 @@
 (ns receive.view.base
   (:require [hiccup.page :as page]
-            [receive.config :as config]))
+            [receive.config :as config]
+            [clojure.data.json :as json]
+            [clojure.walk :refer [stringify-keys]]))
+
+(defn global-config-script []
+  (-> config/config
+      (select-keys [:max-file-size :max-filename-length])
+      (stringify-keys)
+      (json/write-str)
+      (#(format "window.config = %s" %))))
 
 (defn base [children]
   (page/html5
@@ -8,11 +17,12 @@
     [:title (:ui-title config/config)]
     [:meta {:charset "utf-8"}]
     [:meta {:name "theme-color" :content "#5CDb95"}]
-    (page/include-css "css/style.css")]
+    (page/include-css "css/style.css")
+    (page/include-js "js/main.js")
+    [:script (global-config-script)]]
    [:body (if config/staging? {:class "env-staging"} {})
     [:div {:class "container"}
-     children
-     (page/include-js "js/main.js")]]))
+     children]]))
 
 (def title
   [:a {:href "http://receive.nilenso.com"}
@@ -26,4 +36,5 @@
           :name "myForm"}
    [:div {:class "upload-input" :onclick "getFile()"} "Upload"]
    [:div {:style "height: 0px; width: 0px; overflow: hidden"}
-    [:input {:id "upfile" :type "file" :name "file" :value "file" :onchange "uploadFile(this)"}]]])
+    [:input {:id "upfile" :type "file" :name "file" :value "file" :onchange "uploadFile(this)"}]]
+   [:span {:class "upload-error"} "File size is too big!"]])
