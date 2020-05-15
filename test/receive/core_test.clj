1dcf8330a065d4ee768b38d22c5d61864314342c
danySam <danisam17@gmail.com>
2020-05-15 14:19:00 +0530

    mock database out of handler tests

--- test/receive/core_test.clj
+++ test/receive/core_test.clj
@@ -1,9 +1,14 @@
 (ns receive.core-test
   (:require [clojure.java.io :as io]
-            [clojure.test :refer [deftest is]]
+            [clojure.test :as test :refer [deftest is use-fixtures]]
             [receive.core :as core]
             [receive.service.files-test :as files]
+            [receive.service.files :as file-service]
             [ring.mock.request :as mock]))
+
+(def tempfile-name "tempfile.dat")
+(def tempfolder-path "/tmp/")
+(def tempfile-path (str tempfolder-path tempfile-name))
 
 (deftest ping-handler
   (is (= (core/ping (mock/request :get "/ping"))
@@ -22,7 +27,7 @@
    :content-type "text/plain"
    :filename (.getName tempfile)})
 
-(defn mock-response [file]
+(defn mock-upload-response [file]
   (core/upload
    (-> (mock/request :post "/upload/")
        (assoc :content-type "multipart/form-data"
@@ -32,38 +37,58 @@
 
 (deftest upload-handler
   (with-redefs [core/uuid-str
+                (constantly "958a5425-060b-4aad-ba65-bf25e4458991")
+                file-service/save-file
                 (constantly "958a5425-060b-4aad-ba65-bf25e4458991")]
-    (let [tempfile (files/create-temp-file "/tmp/tempfile.dat")
+    (let [tempfile (files/create-temp-file tempfile-path)
           file (tempfile->file tempfile)
-          mock-response (mock-response file)]
+          mock-response (mock-upload-response file)]
       (is (= mock-response
              {:status 200
-              :body {:name "tempfile.dat"
+              :body {:name tempfile-name
                      :success true
                      :message "File saved successfully!"
                      :uid "958a5425-060b-4aad-ba65-bf25e4458991"}})))))
 
 (deftest download-link-test
-  (let [tempfile (files/create-temp-file "/tmp/tempfile.dat")
-        file (tempfile->file tempfile)
-        mock-upload-response (mock-response file)
-        uid (-> mock-upload-response :body :uid)
-        mock-request (-> (mock/request :get (format "/download/%s/" uid))
-                         (assoc :params {:id uid}))
-        mock-response (core/download-view mock-request)
-        headers (:headers mock-response)
-        status (:status mock-response)]
-    (is (= headers {"Content-Type" "text/html"}))
-    (is (= status 200))))
+  (with-redefs [core/uuid-str (constantly "958a5425-060b-4aad-ba65-bf25e4458991")
+                file-service/get-absolute-filename (constantly "/tmp/tempfile.dat")]
+    (let [uid (core/uuid-str)
+          mock-request (-> (mock/request :get (format "/download/api/%s/" uid))
+                           (assoc :params {:id uid}))
+          mock-response (core/download-file mock-request)]
+      (files/create-temp-file tempfile-path)
+      (is (= (:status mock-response) 200))
+      (is (-> mock-response :body (.exists))))))
 
-(deftest download-bad-link-test
-  (let [uid (core/uuid-str)
-        mock-request (mock/request :get (format "/download/%s/" uid))
-        mock-response (core/download-view mock-request)]
-    (is (= mock-response
-           {:status 302, :headers {"Location" "/404"}, :body ""}))))
+(deftest download-ui-link-test
+  (with-redefs [core/uuid-str (constantly "958a5425-060b-4aad-ba65-bf25e4458991")
+                file-service/get-filename (constantly "tempfile.dat")]
+    (let [uid "958a5425-060b-4aad-ba65-bf25e4458991"
+          mock-request (-> (mock/request :get (format "/download/%s/" uid))
+                           (assoc :params {:id uid}))
+          mock-response (core/download-view mock-request)
+          headers (:headers mock-response)
+          status (:status mock-response)]
+      (is (= headers {"Content-Type" "text/html"}))
+      (is (= status 200)))))
+
+(deftest download-ui-bad-link-test
+  (with-redefs [file-service/get-filename (constantly nil)]
+    (let [uid (core/uuid-str)
+          mock-request (mock/request :get (format "/download/%s/" uid))
+          mock-response (core/download-view mock-request)]
+      (is (= mock-response
+             {:status 302, :headers {"Location" "/404"}, :body ""})))))
 
 (deftest index-handler
   (let [mock-response (core/index (mock/request :get "/"))
         mock-status (:status mock-response)]
     (is (= mock-status 200))))
+
+(defn cleanup-tempfile [f]
+  (f)
+  (io/delete-file tempfile-path))
+
+;; TODO: Add functionality to use fixtures per test
+(use-fixtures :once cleanup-tempfile)
