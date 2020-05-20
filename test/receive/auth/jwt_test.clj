0e38c2fe93b7876b7d5eda244435132b87e5ce3a
danySam <danisam17@gmail.com>
2020-05-20 19:25:26 +0530

    add test for jwt sign and verify

---
+++ test/receive/auth/jwt_test.clj
@@ -0,0 +1,13 @@
+(ns receive.auth.jwt-test
+  (:require [clojure.test :refer [deftest is]]
+            [receive.auth.jwt :as jwt]))
+
+(deftest jwt-test
+  (let [user-data {:user-id 100 
+                   :email "john@titor.com"}
+        verified (-> user-data
+            jwt/sign
+            jwt/verify)]
+    (is (= (:user-id user-data) (:user_id verified)))
+    (is (= (:email user-data) (:email verified)))
+    (is (contains? verified :exp))))
