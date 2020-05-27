6c5bcdede8142bb3c5ffc7d8c4eac803444331ec
danySam <danisam17@gmail.com>
2020-05-27 23:48:11 +0530

    add macro to support error handling

--- src/receive/error_handler.clj
+++ src/receive/error_handler.clj
@@ -21,3 +21,19 @@
     {:status (:status response-data)
      :body {:success false
             :message (:message response-data)}}))
+
+(defmacro if-error
+  "Macro. Evaluates test on the data.
+   If error, evaluates `error-condition` otherwise evaluates
+   the `error-condition`
+   
+   `error-condition` can have either an expression or `:raise` or `:http-response`
+   `:raise`           returns the data
+   `:http-response`   returns data wrapped with HTTP respone"
+  [data error-condition else-condition]
+  `(if (error? ~data)
+     (case ~error-condition
+       :raise ~data
+       :http-response (error->http-response ~data)
+       ~error-condition)
+     ~else-condition))
