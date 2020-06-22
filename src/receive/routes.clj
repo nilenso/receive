5bc119810917923e6603dd3351734333804ce38c
danySam <danisam17@gmail.com>
2020-06-23 00:06:40 +0530

    replace middlewares in routes with ->WrapMiddleware

--- src/receive/routes.clj
+++ src/receive/routes.clj
@@ -1,41 +1,38 @@
 (ns receive.routes
-  (:require [bidi.ring :refer [make-handler]]
-            [receive.handlers.api :as api-handlers]
-            [receive.handlers.file :as file-handlers]
-            [receive.handlers.ui :as ui-handlers]
-            [receive.middlewares :refer [wrap-fallback-exception
-                                         wrap-postgres-exception
-                                         wrap-with-uri-rewrite
-                                         trim-trailing-slash
-                                         verified-user
-                                         wrap-cookies-keyword]]
-            [ring.middleware.cookies :refer [wrap-cookies]]
-            [ring.middleware.json :refer [wrap-json-response
-                                          wrap-json-params]]
-            [ring.middleware.params :refer [wrap-params]]
-            [ring.middleware.multipart-params :refer [wrap-multipart-params]]
-            [ring.middleware.resource :refer [wrap-resource]]
-            [ring.middleware.keyword-params :refer [wrap-keyword-params]]
-            [ring.logger :refer [wrap-with-logger]]
-            [ring.util.response :as response]))
+  (:require
+   [bidi.ring :refer [->WrapMiddleware
+                      make-handler]]
+   [receive.handlers.api :as api-handlers]
+   [receive.handlers.file :as file-handlers]
+   [receive.handlers.ui :as ui-handlers]
+   [receive.middlewares :refer [wrap-fallback-exception
+                                wrap-postgres-exception
+                                wrap-with-uri-rewrite
+                                trim-trailing-slash
+                                verified-user
+                                wrap-cookies-keyword]]
+   [ring.middleware.cookies :refer [wrap-cookies]]
+   [ring.middleware.json :refer [wrap-json-response
+                                 wrap-json-params]]
+   [ring.middleware.params :refer [wrap-params]]
+   [ring.middleware.multipart-params :refer [wrap-multipart-params]]
+   [ring.middleware.resource :refer [wrap-resource]]
+   [ring.middleware.keyword-params :refer [wrap-keyword-params]]
+   [ring.logger :refer [wrap-with-logger]]
+   [ring.util.response :as response]))
 
 (def api-routes
-  {"/ping"      (wrap-json-response api-handlers/ping)
-   "/download/" {[:id ""] (wrap-json-response file-handlers/download-file)}
-   "/upload"    {:post {"" (-> file-handlers/upload
-                               (wrap-json-response))}}
-   "/user"      {""        {:put (-> api-handlers/sign-in
-                                     (wrap-json-response))
-                            :get (-> api-handlers/fetch-user
-                                     (wrap-json-response))}
-                 "/files/" {[:id ""]
-                            {:put (wrap-json-response
-                                   api-handlers/update-file)}}}
-   true       (wrap-json-response api-handlers/not-found)})
+  {"/ping"      api-handlers/ping
+   "/download/" {[:id ""] file-handlers/download-file}
+   "/upload"    {:post {"" file-handlers/upload}}
+   "/user"      {""        {:put api-handlers/sign-in
+                            :get api-handlers/fetch-user}
+                 "/files/" {[:id ""] {:put api-handlers/update-file}}}
+   true       api-handlers/not-found})
 
 (def routes
   ["/" {""          file-handlers/index
-        "api"       api-routes
+        "api"       (->WrapMiddleware api-routes wrap-json-response)
         "download/" {[:id ""] file-handlers/download-view}
         "share"     {:get file-handlers/share-handler}
         "404"       {:get ui-handlers/error-page}
