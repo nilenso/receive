cb7cdc369031ce62ffe3f218126da652e6db20eb
danySam <danisam17@gmail.com>
2020-06-24 10:40:45 +0530

    remove redundant tests

--- test/receive/service/files_test.clj
+++ test/receive/service/files_test.clj
@@ -1,17 +1,21 @@
 (ns receive.service.files-test
   (:require
-   [camel-snake-kebab.core :as csk]
    [clojure.test :refer [deftest is testing use-fixtures]]
    [clojure.java.io :as io]
    [receive.handlers.file :refer [uuid-str]]
-   [next.jdbc.sql :refer [insert! delete! find-by-keys]]
+   [next.jdbc.sql :refer [find-by-keys]]
    [receive.db.connection :refer [datasource]]
-   [receive.factory :as factory]
+   [receive.model.file :as model]
    [receive.service.files :as files])
   (:import (java.util UUID)))
 
 (def ^:dynamic *tempfile* nil)
-(def ^:dynamic *file-data* nil)
+
+(def file-data
+  {:filename "xjU5T7SpfhdvbpB5AOnYo"
+   :uid #uuid "93dd0920-b5d7-11ea-a650-4c32759dd39d"
+   :dt_created #inst "2020-06-24T05:00:09.098531000-00:00"
+   :dt_expire nil})
 
 (defn tempfile-location [filename]
   (str "/tmp/" filename))
@@ -20,9 +24,11 @@
   (find-by-keys datasource :file_storage {:uid (UUID/fromString uid)}))
 
 (deftest get-filename-test
-  (testing "should return an existing file"
-    (is (= (files/get-filename (str (:file_storage/uid *file-data*)))
-           (:file_storage/filename *file-data*))))
+  (with-redefs [model/find-file
+                (constantly (select-keys file-data  [:filename :uid]))]
+    (testing "should return an existing file"
+      (is (= (files/get-filename (str (:uid file-data)))
+             (:filename file-data)))))
   (testing "should return error when file does not exists"
     (is (= (files/get-filename (uuid-str))
            {:error :not-found}))))
@@ -30,18 +36,13 @@
 (deftest save-file-to-disk
   (testing "should save file to the specified location"
     (let [filename (tempfile-location
-                    (:file_storage/filename *file-data*))]
+                    (:filename file-data))]
       (files/save-to-disk *tempfile* filename)
       (is (.exists (io/file filename))))))
 
 (deftest find-file-bad-uid-test
   (is (= (files/get-filename (uuid-str))
          {:error :not-found})))
-
-(defn keywords->sql-keywords [data]
-  (into {}
-        (for [[k v] data]
-          [(csk/->snake_case k) v])))
 
 (defn create-temp-file
   "Creates a file given a filename"
@@ -53,23 +54,13 @@
 (defn delete-tempfile [file]
   (io/delete-file file))
 
-(defn create-file [file-data]
-  (insert! datasource :file_storage
-           (keywords->sql-keywords file-data)))
-
-(defn delete-file [{uid :file_storage/uid}]
-  (delete! datasource :file_storage {:uid uid}))
-
 (defn file-fixture [f]
-  (let [file-data (create-file (factory/generate-file))
-        tempfile (create-temp-file
+  (let [tempfile (create-temp-file
                   (tempfile-location
-                   (:file_storage/filename file-data)))]
-    (binding [*tempfile* tempfile
-              *file-data* file-data]
+                   (:filename file-data)))]
+    (binding [*tempfile* tempfile]
       (f))
-    (delete-tempfile tempfile)
-    (delete-file file-data)))
+    (delete-tempfile tempfile)))
 
 (use-fixtures :each  file-fixture)
 
@@ -77,6 +68,4 @@
      (create-temp-file
       (str "/tmp/"
            (:file_storage/filename
-            (create-file (factory/generate-file))))))
-   (def ^:dynamic *file-data*
-     (create-file (factory/generate-file))))
+            (create-file (factory/generate-file)))))))
